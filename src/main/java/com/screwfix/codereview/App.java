c92bb56de9eca9da21fd8981f00e7e599992912b
Andrii Artemenko <Andrii_Artemenko@epam.com>
2016-12-08 17:48:35 +0200

    Example

---
+++ src/main/java/com/screwfix/codereview/App.java
@@ -0,0 +1,28 @@
+package com.screwfix.codereview;
+
+public class App {
+
+	private static double pi() {
+		int count = 999999999;
+		double pi = 0;
+		double denominator = 1;
+
+		for (int x = 0; x < count; x++) {
+			if (x % 2 == 0) {
+				pi = pi + (1 / denominator);
+			} else {
+				pi = pi - (1 / denominator);
+			}
+			denominator = denominator + 2;
+		}
+		pi = pi * 4;
+
+		return pi;
+	}
+
+	public static void main(String[] args) {
+		double value = pi();
+		System.out.println("Pi = " + value);
+	}
+
+}
