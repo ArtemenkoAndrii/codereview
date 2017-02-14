98308a7b3eba5c323c7de8eed20e1cb881e88f3d
Andrii Artemenko <andrii_artemenko@epam.com>
2017-02-14 17:49:40 +0200

    Check for Sonar

--- src/main/java/com/screwfix/codereview/App.java
+++ src/main/java/com/screwfix/codereview/App.java
@@ -2,39 +2,8 @@ package com.screwfix.codereview;
 
 public class App {
 
-	private static double pi() {
-		int count = 999999999;
-		double pi = 0;
-		double denominator = 1;
-
-		for (int x = 0; x < count; x++) {
-			if (x % 2 == 0) {
-				pi = pi + (1 / denominator);
-			} else {
-				pi = pi - (1 / denominator);
-			}
-			denominator = denominator + 2;
-		}
-		pi = pi * 4;
-		
-		if (1 == 1) {
-			if (2 == 2) {
-				if (3 ==3 ) {
-					if (4 == 4) {
-						System.out.println("Refactor me");
-					}	
-				}	
-			}
-		}	
-
-		return pi;
-	}
-
 	public static void main(String[] args) {
-		double value = pi();
-		System.out.println("Pi = " + value);
+		System.out.println("Hello word!");
 	}
-	
-	//Commit 1
 
 }
