5e3e2de0ca240f1dea4403b8e09256d11110ba0c
Andrii Artemenko <andrii_artemenko@epam.com>
2017-04-24 12:53:03 +0300

    Revert "Pre-test"
    This reverts commit 3f4af3cfc501829d2295c53ab54b9c5ca07b50a2.

--- src/main/java/com/screwfix/codereview/App.java
+++ src/main/java/com/screwfix/codereview/App.java
@@ -2,32 +2,8 @@ package com.screwfix.codereview;
 
 public class App {
 
-	private static double pi() {
-		int count = 999999999;
-		double pi = 0;
-		double denominator = 1;
-
-		for (int x = 0; x < count; x++) {
-			if (x % 2 == 0) {
-				pi = pi + (1 / denominator);
-			} else {
-				pi = pi - (1 / denominator);
-			}
-			denominator = denominator + 2;
-		}
-		pi = pi * 4;
-		
-		if (1 == 1) {
-			if (2 == 2) {
-				if (3 ==3 ) {
-					if (4 == 4) {
-						System.out.println("Please, finish me off...");
-					}	
-				}	
-			}
-		}
-
-		return pi;
+	private static double pi() {	
+		return 3.14;
 	}
 
 	public static void main(String[] args) {
